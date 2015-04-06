5d3a432146eeea2e7b43b6142e7bc8b5f4a4d57a
Piyush Verma <github@piyushverma.net>
2015-04-06 18:17:16 +0530

    Fixed the sc-messages filter

--- validate.sh
+++ validate.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-FILES=notifications/translations/*
+FILES=notifications/translations/*.json
 for f in $FILES
 do
+  echo "Parsing ${f}"
   output=`cat $f | python -m "json.tool" 2>&1`
   if [ $? -gt 0 ]; then
     echo "Failed to Parse $f. Reason: $output"
